8a77f064c60ff314c3f01898eb3de5f90d62b40d
lubaanah <lubaanah@gmail.com>
2025-01-25 14:51:04 +0530

    second commit

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        // Reverse the string
+        String re
